46bcf448a1bd5c7ee5b665ce1948369f001d5c9b
li2he1 <peterli2he1@gmail.com>
2020-02-29 12:38:04 -0500

    Simple Voice Control

--- app/src/main/java/com/laioffer/matrix/MainFragment.java
+++ app/src/main/java/com/laioffer/matrix/MainFragment.java
@@ -3,6 +3,7 @@ package com.laioffer.matrix;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -19,6 +20,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,6 +57,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -65,6 +69,7 @@ import static android.app.Activity.RESULT_OK;
 
 public class MainFragment extends Fragment implements OnMapReadyCallback, ReportDialog.DialogCallBack, GoogleMap.OnMarkerClickListener {
     private static final int REQUEST_CAPTURE_IMAGE = 100;
+    private static final int REQ_CODE_SPEECH_INPUT = 101;
     private final String path = Environment.getExternalStorageDirectory() + "/temp.png";
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -79,6 +84,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
     private FloatingActionButton fabReport;
     private ReportDialog dialog;
     private FloatingActionButton fabFocus;
+    private FloatingActionButton speakNow;
     private DatabaseReference database;
     private FirebaseStorage storage;
     private StorageReference storageRef;
@@ -148,6 +154,15 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
             }
         });
 
+        speakNow = view.findViewById(R.id.voice);
+        speakNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askSpeechInput("Hi speak something");
+            }
+        });
+
+
 
         if (mapView != null) {
             mapView.onCreate(null);
@@ -155,6 +170,22 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
             mapView.getMapAsync(this);
         }
     }
+
+    private void askSpeechInput(String string) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 2000);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                string);
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+
 
     @Override
     public void onResume() {
@@ -183,8 +214,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
     private void showDialog(String label, String prefillText) {
         int cx = (int) (fabReport.getX() + (fabReport.getWidth() / 2));
         int cy = (int) (fabReport.getY()) + fabReport.getHeight() + 56;
-        dialog = ReportDialog.newInstance(getContext(), cx, cy, this);
-
+        dialog = ReportDialog.newInstance(getContext(), cx, cy, this, label, prefillText);
         dialog.show();
     }
 
@@ -295,6 +325,30 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
                         e.printStackTrace();
                     }
                 }
+                break;
+            }
+            case REQ_CODE_SPEECH_INPUT: {
+                showDialog("Traffic", "there is a traffic ahead");
+//                if (resultCode == RESULT_OK && null != data) {
+//
+//                    ArrayList<String> result = data
+//                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    if (result.size() > 0) {
+//                        final String sentence = result.get(0);
+//                        boolean isMatch = false;
+//                        for (int i = 0; i < listItems.size(); i++) {
+//                            final String label = listItems.get(i).getDrawable_label();
+//                            if (sentence.toLowerCase().contains(label.toLowerCase())) {
+//                                showDialog(label, sentence);
+//                                isMatch = true;
+//                                break;
+//                            }
+//                        }
+//                        if (!isMatch) {
+//                            askSpeechInput("Try again");
+//                        }
+//                    }
+//                }
                 break;
             }
             default:
